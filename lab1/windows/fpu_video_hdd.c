79003316ef664ded22449470558795c8c4551496
pistophone <=>
2026-03-01 17:58:56 +0500

    lab1 windows done

--- lab1/windows/fpu_video_hdd.c
+++ lab1/windows/fpu_video_hdd.c
@@ -6,9 +6,16 @@ const int million = 1000 * 1000;
 int main() {
   setvbuf(stdout, NULL, _IOFBF, BUFSIZ);
   double result = 0;
-  for (int i = 0; i < 2 * million; i++) {
+  FILE *f = fopen("result.txt", "w");
+  if (f == NULL) {
+    puts("error writing to file");
+    return 1;
+  }
+  for (int i = 0; i < 200000; i++) {
     result += i * 3 + sin(i) - sqrt(1.f / 15);
     printf("%lf\n", result);
+    fprintf(f, "%lf\n", result);
   }
+  fclose(f);
   return 0;
 }
--- lab1/windows/fpu_video_hdd_fsync.c
+++ lab1/windows/fpu_video_hdd_fsync.c
@@ -7,19 +7,20 @@
 const int million = 1000 * 1000;
 
 int main() {
+  setvbuf(stdout, NULL, _IOFBF, BUFSIZ);
   double result = 0;
   FILE *f = fopen("result.txt", "w");
   if (f == NULL) {
     puts("error writing to file");
     return 1;
   }
-  //HANDLE hFile = (HANDLE)_get_osfhandle(fileno(f));
+  HANDLE hFile = (HANDLE)_get_osfhandle(fileno(f));
   for (int i = 0; i < 20000; i++) {
     result += i * 3 + sin(i) - sqrt(1.f / 15);
+    printf("%lf\n", result);
     fprintf(f, "%lf\n", result);
     fflush(f);
-    _commit(fileno(f));
-    //FlushFileBuffers(hFile);
+    FlushFileBuffers(hFile);
   }
   fclose(f);
   return 0;
